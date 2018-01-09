e1c5b6c619c22673f5cc30ad3761a2338267979f
AnLe1 <you@example.com>
2018-01-09 11:01:36 +0700

    commit changed

---
+++ app/src/main/java/com/example/admin/myapp/LoginActivity.java
@@ -0,0 +1,10 @@
+package com.example.admin.myapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Admin on 09/01/2018.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+}
